59165a94cf14d11d821590ee6801173c9988a2a2
Tim Henning <tim@luminosuslight.de>
2020-02-03 13:59:54 +0100

    Enable https and IPv6 on server part

--- server/start.sh
+++ server/start.sh
@@ -1,5 +1,5 @@
 eval "$(conda shell.bash hook)"
 conda activate fastai_env
 export FLASK_APP=main.py
-python3 -m flask run --host=0.0.0.0 --port=5000
+python3 -m flask run --host='::' --port=5000 --cert=adhoc
 conda deactivate 
