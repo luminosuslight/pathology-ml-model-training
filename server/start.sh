0db36db6a29d1b0764d3ecd8c249b6298c50da93
Tim Henning <tim@luminosuslight.de>
2020-02-03 15:11:09 +0100

    Change port to 55712 and add default server url

--- server/start.sh
+++ server/start.sh
@@ -1,5 +1,5 @@
 eval "$(conda shell.bash hook)"
 conda activate fastai_env
 export FLASK_APP=main.py
-python3 -m flask run --host='::' --port=5000 --cert=luminosus_websocket.cert --key=luminosus_websocket.key
+python3 -m flask run --host='::' --port=55712 --cert=luminosus_websocket.cert --key=luminosus_websocket.key
 conda deactivate 
