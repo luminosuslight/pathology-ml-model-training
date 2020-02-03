ce0a5a44654b221c58d085705255f57694812c8b
Tim Henning <tim@luminosuslight.de>
2020-02-03 14:45:21 +0100

    Use self-signed SSL certificate for Flask server

--- server/start.sh
+++ server/start.sh
@@ -1,5 +1,5 @@
 eval "$(conda shell.bash hook)"
 conda activate fastai_env
 export FLASK_APP=main.py
-python3 -m flask run --host='::' --port=5000 --cert=adhoc
+python3 -m flask run --host='::' --port=5000 --cert=luminosus_websocket.cert --key=luminosus_websocket.key
 conda deactivate 
